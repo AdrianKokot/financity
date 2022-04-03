68ee52d3966dfd9cfda00237e4561e36b18b90d1
Adrian Kokot <splateusz@gmail.com>
2022-04-03 21:07:36 +0200

    Override SaveChangesAsync in order to set proper properties on AuditableEntities

--- api/Domain/Common/AuditableEntity.cs
+++ api/Domain/Common/AuditableEntity.cs
@@ -4,5 +4,7 @@ public class AuditableEntity
 {
     public int Id { get; set; }
     public DateTime CreatedAt { get; set; }
+    public string CreatedBy { get; set; }
     public DateTime? UpdatedAt { get; set; }
+    public string UpdatedBy { get; set; }
 }
--- api/Persistence/ApplicationDbContext.cs
+++ api/Persistence/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Financity.Domain.Common;
+using Microsoft.EntityFrameworkCore;
 
 namespace Financity.Persistence;
 
@@ -6,5 +7,25 @@ public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        foreach (var entry in ChangeTracker.Entries<AuditableEntity>())
+        {
+            switch (entry.State)
+            {
+                case EntityState.Added:
+                    entry.Entity.CreatedAt = DateTime.UtcNow;
+                    entry.Entity.CreatedBy = string.Empty;
+                    break;
+                case EntityState.Modified:
+                    entry.Entity.UpdatedAt = DateTime.UtcNow;
+                    entry.Entity.UpdatedBy = string.Empty;
+                    break;
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
     }
 }
