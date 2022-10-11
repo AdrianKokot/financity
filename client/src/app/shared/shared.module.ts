b98a1b8ea101c005235a99e9649fc8652025ae82
Adrian Kokot <splateusz@gmail.com>
2022-10-11 16:14:28 +0200

    Add Taiga UI

---
+++ client/src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TuiButtonModule } from '@taiga-ui/core';
+
+const sharedModules = [
+  TuiButtonModule
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ...sharedModules
+  ],
+  exports: [
+    ...sharedModules
+  ]
+})
+export class SharedModule { }
