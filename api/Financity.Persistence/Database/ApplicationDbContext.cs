3219a1de0be8197122930ca9b7357ead39dad696
Adrian Kokot <splateusz@gmail.com>
2022-10-17 13:16:14 +0200

    Configure docker

--- api/Financity.Persistence/Database/ApplicationDbContext.cs
+++ api/Financity.Persistence/Database/ApplicationDbContext.cs
@@ -42,8 +42,6 @@ public class ApplicationDbContext : DbContext, IApplicationDbContext
                     entry.Entity.UpdatedAt = AppDateTime.Now;
                     entry.Entity.UpdatedBy = string.Empty;
                     break;
-                default:
-                    break;
             }
         }
 
