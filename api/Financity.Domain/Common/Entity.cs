0aa938f29ca766a8717bd45ed9055955e56c7300
Adrian Kokot <splateusz@gmail.com>
2022-11-01 19:55:29 +0100

    Add Equals method to Entity
    Fix CS-R1078 error: abstract classes with only abstract methods should be declared as interfaces

--- api/Financity.Domain/Common/Entity.cs
+++ api/Financity.Domain/Common/Entity.cs
@@ -6,10 +6,25 @@ public abstract class Entity
     {
     }
 
-    protected Entity(Guid id)
+    public Entity(Guid id)
     {
         Id = id;
     }
 
-    public Guid Id { get; init; }
+    public Guid Id { get; private init; }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null || obj.GetType() != GetType() || obj is not Entity entity)
+        {
+            return false;
+        }
+
+        return entity.Id == Id;
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
 }
