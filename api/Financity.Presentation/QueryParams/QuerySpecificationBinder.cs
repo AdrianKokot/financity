1b016ac0c83cfebdf6097f0a7dd8f984561214a9
Adrian Kokot <splateusz@gmail.com>
2022-11-21 15:18:07 +0100

    Add entity filters binding

--- api/Financity.Presentation/QueryParams/QuerySpecificationBinder.cs
+++ api/Financity.Presentation/QueryParams/QuerySpecificationBinder.cs
@@ -1,44 +1,73 @@
 ﻿using System.ComponentModel;
+using System.Reflection;
 using Financity.Application.Common.Queries;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Financity.Presentation.QueryParams;
 
-public static class QueryParamKeys
+public static class QueryKeys
 {
     public const string PageSizeQueryParamKey = "pageSize";
     public const string PageQueryParamKey = "page";
     public const string OrderByQueryParamKey = "orderBy";
     public const string OrderByDirectionQueryParamKey = "direction";
 
-    public static readonly HashSet<Type> AllowedFilterKeyTypes = new() {typeof(Guid), typeof(string), typeof(int)};
+    public static readonly HashSet<Type> AllowedFilterKeyTypes = new() {typeof(Guid), typeof(string), typeof(DateTime)};
+}
+
+public static class QueryParamFilters
+{
+    public static readonly IDictionary<Type, IEnumerable<string>> TypeOperators =
+        new Dictionary<Type, IEnumerable<string>>()
+        {
+            {typeof(Guid), new[] {"eq"}},
+            {typeof(string), new[] {"eq", "ct"}},
+            {typeof(DateTime), new[] {"gte", "lte"}},
+        };
+
+    public static readonly IDictionary<string, string> OperatorFunctions = new Dictionary<string, string>()
+    {
+        {"eq", "Equals"},
+        {"ct", "Contains"}
+    };
+
+    public static readonly HashSet<string> Operators = new()
+    {
+        "eq", "ct", "gte", "lte"
+    };
 }
 
 public sealed class QuerySpecificationBinder<T> : IModelBinder
 {
     private readonly IObjectModelValidator _validator;
+    private readonly IDictionary<string, PropertyInfo> _entityProperties;
 
     public QuerySpecificationBinder(IObjectModelValidator validator)
     {
         _validator = validator;
+        _entityProperties = typeof(T)
+                            .GetProperties()
+                            .Where(x => QueryKeys.AllowedFilterKeyTypes.Contains(x.PropertyType))
+                            .ToDictionary(x => x.Name.ToLower(), x => x);
     }
 
     private static PaginationSpecification ParsePagination(IValueProvider valueProvider)
     {
         var specification = new PaginationSpecification();
 
-        var pageSizeString = valueProvider.GetValue(QueryParamKeys.PageSizeQueryParamKey).FirstValue;
+        var pageSizeString = valueProvider.GetValue(QueryKeys.PageSizeQueryParamKey).FirstValue;
 
         if (pageSizeString is not null)
         {
             specification.Take = int.Parse(pageSizeString);
         }
 
-        var pageString = valueProvider.GetValue(QueryParamKeys.PageQueryParamKey).FirstValue;
+        var pageString = valueProvider.GetValue(QueryKeys.PageQueryParamKey).FirstValue;
         if (pageString is not null)
         {
             specification.Skip = specification.Take * Math.Clamp(int.Parse(pageString) - 1, 0, int.MaxValue);
@@ -51,15 +80,14 @@ public sealed class QuerySpecificationBinder<T> : IModelBinder
     {
         var specification = new SortSpecification();
 
-        var orderByString = (valueProvider.GetValue(QueryParamKeys.OrderByQueryParamKey).FirstValue ?? "id").ToLower();
+        var orderByString = (valueProvider.GetValue(QueryKeys.OrderByQueryParamKey).FirstValue ?? string.Empty)
+            .ToLower();
 
-        specification.OrderBy = typeof(T)
-                                .GetProperties()
-                                .FirstOrDefault(x =>
-                                    QueryParamKeys.AllowedFilterKeyTypes.Contains(x.PropertyType) &&
-                                    x.Name.ToLower() == orderByString)?.Name ?? "Id";
+        specification.OrderBy = _entityProperties.TryGetValue(orderByString, out var info) ? info.Name : "Id";
 
-        var directionString = valueProvider.GetValue(QueryParamKeys.OrderByDirectionQueryParamKey).FirstValue ?? string.Empty;
+        var directionString = valueProvider.GetValue(QueryKeys.OrderByDirectionQueryParamKey).FirstValue ??
+                              string.Empty;
+
         specification.Direction = directionString.ToLower().StartsWith("desc")
             ? ListSortDirection.Descending
             : ListSortDirection.Ascending;
@@ -67,21 +95,44 @@ public sealed class QuerySpecificationBinder<T> : IModelBinder
         return specification;
     }
 
+    private IEnumerable<Filter> ParseFilters(ModelBindingContext bindingContext)
+    {
+        return bindingContext.HttpContext.Request.Query.Keys
+                             .Where(x => x.Contains('_'))
+                             .Select(x =>
+                             {
+                                 var operatorString = x[..x.IndexOf('_')].ToLower();
+                                 var propertyNameString = x[(operatorString.Length + 1)..].ToLower();
+
+                                 if (!_entityProperties.TryGetValue(propertyNameString, out var property))
+                                     return null;
+
+                                 if (!QueryParamFilters.TypeOperators[property.PropertyType]
+                                                       .Contains(operatorString))
+                                     return null;
+
+                                 return new Filter()
+                                 {
+                                     Key = property.Name,
+                                     Operator = operatorString,
+                                     Value = bindingContext.ValueProvider.GetValue(x).FirstValue!
+                                 };
+                             })
+                             .Where(x => x is not null)
+                             .ToList()!;
+    }
+
     public async Task BindModelAsync(ModelBindingContext bindingContext)
     {
-        var keys = bindingContext.HttpContext.Request.Query.Keys.ToList();
-
         var model = new QuerySpecification<T>()
         {
             Pagination = ParsePagination(bindingContext.ValueProvider),
-            Sort = ParseSort(bindingContext.ValueProvider)
+            Sort = ParseSort(bindingContext.ValueProvider),
+            Filters = ParseFilters(bindingContext)
         };
 
         bindingContext.Result = ModelBindingResult.Success(model);
         _validator.Validate(bindingContext.ActionContext, bindingContext.ValidationState, string.Empty, model);
-
-        Console.WriteLine((model.Pagination.Skip, model.Pagination.Take));
-        Console.WriteLine((model.Sort.OrderBy, model.Sort.Direction));
 
         await Task.CompletedTask;
     }
--- api/Financity.Presentation/QueryParams/QuerySpecificationFilter.cs
+++ api/Financity.Presentation/QueryParams/QuerySpecificationFilter.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Mime;
+using System.Text.Json;
 using Financity.Application.Common.Queries;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
@@ -10,44 +11,65 @@ public sealed class QuerySpecificationFilter : IOperationFilter
     private static readonly IDictionary<Type, OpenApiSchema> ApiSchemas = new Dictionary<Type, OpenApiSchema>()
     {
         {typeof(int), new OpenApiSchema() {Type = "integer", Format = nameof(Int32).ToLower()}},
-        {typeof(string), new OpenApiSchema() {Type = nameof(String).ToLower()}}
+        {typeof(string), new OpenApiSchema() {Type = nameof(String).ToLower()}},
+        {typeof(Guid), new OpenApiSchema() {Type = nameof(String).ToLower(), Format = "uuid"}},
+        {typeof(DateTime), new OpenApiSchema() {Type = nameof(String).ToLower(), Format = "date-time"}}
     };
 
     public void Apply(OpenApiOperation operation, OperationFilterContext context)
     {
-        var hasQuerySpecificationBinding = context.ApiDescription.ActionDescriptor.Parameters
-                                                  .Any(x => x.ParameterType.IsGenericType &&
-                                                            x.ParameterType
-                                                             .GetGenericTypeDefinition() == typeof(QuerySpecification<>)
-                                                  );
+        var parameterDescriptor = context.ApiDescription.ActionDescriptor.Parameters
+                                         .FirstOrDefault(x => x.ParameterType.IsGenericType &&
+                                                              x.ParameterType
+                                                               .GetGenericTypeDefinition() ==
+                                                              typeof(QuerySpecification<>)
+                                         );
 
-        if (!hasQuerySpecificationBinding)
+        if (parameterDescriptor is null)
         {
             return;
         }
 
-        var querySpecificationProperties = typeof(QuerySpecification<>).GetProperties().Select(x => x.Name).ToHashSet();
+        var entityType = parameterDescriptor.ParameterType.GetGenericArguments().FirstOrDefault();
+
+        var querySpecificationProperties = typeof(QuerySpecification<>)
+                                           .GetProperties()
+                                           .Select(x => x.Name)
+                                           .ToHashSet();
 
         operation.Parameters = operation.Parameters
                                         .Where(x => !querySpecificationProperties.Contains(x.Name.Split(".").First()))
                                         .ToList();
-        
-        (Type Type, string Name)[] integerBindings =
+
+        var bindings = new List<KeyValuePair<Type, string>>()
         {
-            (typeof(int), QueryParamKeys.PageQueryParamKey),
-            (typeof(int), QueryParamKeys.PageSizeQueryParamKey),
-            (typeof(string), QueryParamKeys.OrderByQueryParamKey),
-            (typeof(string), QueryParamKeys.OrderByDirectionQueryParamKey),
+            new(typeof(int), QueryKeys.PageQueryParamKey),
+            new(typeof(int), QueryKeys.PageSizeQueryParamKey),
+            new(typeof(string), QueryKeys.OrderByQueryParamKey),
+            new(typeof(string), QueryKeys.OrderByDirectionQueryParamKey),
         };
 
-        foreach (var binding in integerBindings)
+        if (entityType is not null)
+        {
+            var filters = entityType.GetProperties()
+                                    .Where(x => QueryKeys.AllowedFilterKeyTypes.Contains(x.PropertyType))
+                                    .Select(x =>
+                                        (x.PropertyType, Name: JsonNamingPolicy.CamelCase.ConvertName(x.Name))
+                                    )
+                                    .SelectMany(p => QueryParamFilters.TypeOperators[p.PropertyType].Select(o =>
+                                        new KeyValuePair<Type, string>(p.PropertyType, p.Name + "_" + o)));
+
+            bindings.AddRange(filters);
+        }
+
+        foreach (var binding in bindings)
         {
             operation.Parameters.Add(new OpenApiParameter()
             {
-                Name = binding.Name,
+                Name = binding.Value,
                 In = ParameterLocation.Query,
                 Required = false,
-                Schema = ApiSchemas[binding.Type]
+                Schema = ApiSchemas[binding.Key]
             });
         }
     }
