6b34e772d01a77d55704a905d59cb951477efc52
Adrian Kokot <splateusz@gmail.com>
2022-12-28 12:18:13 +0100

    Fix wallet sharing

--- api/Financity.Presentation/Middleware/ExceptionHandlingMiddleware.cs
+++ api/Financity.Presentation/Middleware/ExceptionHandlingMiddleware.cs
@@ -86,7 +86,7 @@ public sealed class ExceptionHandlingMiddleware : IMiddleware
             model.AddModelError(string.Empty, formatException.Message);
 
         if (exception is EntityAlreadyExistsException existsException)
-            model.AddModelError(existsException.PropertyName, existsException.Message);
+            model.AddModelError(JsonNamingPolicy.CamelCase.ConvertName(existsException.PropertyName), existsException.Message);
 
         return model;
     }
